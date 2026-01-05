ef911893c41d455791f9e17a83d02ec10e06dbaa
suyash <suyashtripathi944@gmail.com>
2026-01-06 03:12:12 +0530

    Time: 4 ms (5.97%), Space: 50.8 MB (5.11%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,15 +1,93 @@
 class Solution {
     public void reverseString(char[] s) {
-        int st = 0;
-        int l = s.length-1;
-        while(st<l){
-            char temp = s[st];
-            s[st] = s[l];
-            s[l] = temp;
-            
-            st++;
-            l--;
+        Stack<Character>stack = new Stack<>();
+        for(int i=0;i<s.length;i++){
+            stack.push(s[i]);
+
+        }
+        char[] res =new char[s.length-1];
+        for(int i=0;i<s.length;i++){
+            s[i] =stack.pop();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//         Stack<Character>stack = new Stack<>();
+//         for(int i=0;i<s.length;i++){
+//             stack.push(s[i]);
+//         }
+//         char[] result = new char[s.length-1];
+//         for(int i =0;i<s.length;i++){
+//             s[i] =stack.pop();
+//         }
+            
+        
+    
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         int left =0;
+//         int right = s.length-1;
+//         while(left < right){
+//             char temp = s[left];
+//             s[left] = s[right];
+//             s[right] = temp;
+//             left ++;
+//             right--;
+
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         int left = 0;
+//         int right= s.length-1;
+//         while(left < right){
+//             char temp = s[left];
+//             s[left] = s[right];
+//             s[right] = temp;
+//             left++;
+//             right--;
+//         }
+
+//     }
+// }
 
