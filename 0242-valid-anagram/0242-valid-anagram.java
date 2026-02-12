eaab4eb29cbe732114b0a77b3b2d6f880f8f8ed0
suyash <suyashtripathi944@gmail.com>
2026-02-12 11:33:47 +0530

    Time: 5 ms (58.67%), Space: 44.3 MB (89.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,16 +4,15 @@ class Solution {
             return false;
         }
         int[] count = new int[26];
-        for(int i = 0;i<s.length();i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a'] --;
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i) -'a']++;
+            count[t.charAt(i) -'a']--;
         }
-        for(int i=0;i<26;i++){
-            if(count[i] != 0){
+        for(int i=0;i<count.length;i++){
+            if(count[i]!= 0){
                 return false;
             }
         }
         return true;
-        
     }
 }
