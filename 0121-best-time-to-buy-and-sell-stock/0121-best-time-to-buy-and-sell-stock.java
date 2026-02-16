89b7bd01d7327bef0384a560d6314a245e01b899
suyash <suyashtripathi944@gmail.com>
2026-02-16 16:01:15 +0530

    Time: 2 ms (41.7%), Space: 94.5 MB (50.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -6,8 +6,38 @@ class Solution {
             if(prices[i] < min){
                 min = prices[i];
             }
-            profit = Math.max(profit,prices[i] - min);
+            profit = Math.max(profit,prices[i]-min);
         }
         return profit;
     }
 }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         int min = prices[0];
+//         int profit = 0;
+//         for(int i=1;i<prices.length;i++){
+//             if(prices[i] < min){
+//                 min = prices[i];
+//             }
+//             profit = Math.max(profit,prices[i] - min);
+//         }
+//         return profit;
+//     }
+// }
