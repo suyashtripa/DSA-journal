6e60e2684e380b426b62a0ca6378648ebb5e7876
suyash <suyashtripathi944@gmail.com>
2026-02-17 23:44:20 +0530

    Time: 3 ms (87.57%) | Memory: 43.5 MB (41.85%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,23 +3,45 @@ import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
                 stack.push(ch);
-            } else {
-                if (stack.isEmpty()) return false;
+            }
+            else{
+                if(stack.isEmpty()) return false;
                 char top = stack.pop();
-
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
+                if((ch == ')' && top != '(')|| 
+                (ch == '}' && top!='{')||
+                (ch == ']' && top!='[')){
                     return false;
                 }
             }
         }
-
-        return stack.isEmpty();  
+        return stack.isEmpty();
     }
 }
+
+
+
+
+//         Stack<Character> stack = new Stack<>();
+
+//         for (char ch : s.toCharArray()) {
+//             if (ch == '(' || ch == '{' || ch == '[') {
+//                 stack.push(ch);
+//             } else {
+//                 if (stack.isEmpty()) return false;
+//                 char top = stack.pop();
+
+//                 if ((ch == ')' && top != '(') ||
+//                     (ch == '}' && top != '{') ||
+//                     (ch == ']' && top != '[')) {
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return stack.isEmpty();  
+//     }
+// }
 
