1b5bc966b2631c26a48d67f3694ab55b405cb679
suyash <suyashtripathi944@gmail.com>
2026-04-29 18:31:19 +0530

    Time: 3 ms (56.18%), Space: 47.2 MB (40.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -13,37 +13,3 @@ class Solution {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         HashMap<Integer,Integer> hashbox = new HashMap<>();
-//         for(int i=0;i<nums.length;i++){
-//             int diff = target-nums[i];
-//             if(hashbox.containsKey(diff)){
-//                 return new int[]{hashbox.get(diff),i};
-//             }
-//             hashbox.put(nums[i],i);
-//         }
-//         return null;
-//     }
-// }
