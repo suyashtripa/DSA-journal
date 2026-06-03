5cc1fce575fa9594d0fc2d7650522b9891911fec
suyash <suyashtripathi944@gmail.com>
2026-06-03 16:10:59 +0530

    Time: 2552 ms (5.00%) | Memory: 41.7 MB (95.53%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numTrees(int n) {
+        return trees(1,n);
+        
+    }
+    int trees(int lo, int hi){
+        if(lo>=hi) return 1;
+        int total = 0;
+        for(int i=lo;i<=hi;i++){
+            total+=trees(lo,i-1)*trees(i+1,hi);
+        }
+        return total;
+    }
+}
