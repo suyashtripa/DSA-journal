67e12a786b717e98f19d0dbf72df0a6ae69c0497
suyash <suyashtripathi944@gmail.com>
2026-07-26 21:04:34 +0530

    Time: 2 ms (88.86%), Space: 48.1 MB (8.71%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        
+
         int goal = nums.length-1;
         for(int i=nums.length-2;i>=0;i--){
             if(i+nums[i] >= goal){
